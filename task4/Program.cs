ec2d42c2871709f1428e961b1aecfe80fe5c57a4
asel_creator <temerbaevatk@gmail.com>
2024-01-03 01:06:27 +0600

    add lesson1:task4

---
+++ task4/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 14;
+int b = 5;
+int c = 10;
+int d = 7;
+int e = 8;
+int max = a;
+if (b > max){
+    max = b;
+}
+if (c > max){
+    max = c;
+}
+if (d > max){
+    max = d;
+}
+if (e > max){
+    max = e;
+}
+Console.WriteLine(max);
